155c7d9daea30d20d8eae43629df6d45a5676142
arlechann <dragnov3728@gmail.com>
2019-12-13 02:30:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# What is this
+
+This is chrome extension to add twitter share button into address bar.
+
+# Thanks
+
+A function of "tweet" in script.js is written by [@munieru_jp](https://qiita.com/munieru_jp/items/24a4840c452c61c2dde9).
+
+Get a icon image from [Twitterのブランドリソース](https://about.twitter.com/ja/company/brand-resources.html).
---
+++ src/script.js
@@ -0,0 +1,17 @@
+// 
+// A function "tweet" is written by @munieru_jp
+// https://qiita.com/munieru_jp/items/24a4840c452c61c2dde9
+//
+
+const tweet = () => {
+	var w = 550, h = 420;
+	window.open("https://twitter.com/intent/tweet?status=" + encodeURIComponent(document.title) + " " + encodeURIComponent(location.href),
+		"_blank",
+		"width=" + w + ",height=" + h + ",left=" + (screen.width - w) / 2 + ",top=" + (screen.height - h) / 2 + ",scrollbars=yes,resizable=yes,toolbar=no,location=yes");
+};
+
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
+	if (request == "tweet") {
+		tweet();
+	}
+});
